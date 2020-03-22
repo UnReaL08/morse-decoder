731f91d12757db71cf924385a69e0433ad568e27
UnReaL08 <60399065+UnReaL08@users.noreply.github.com>
2020-03-22 23:35:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,10 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
 
+function decode(str) {
+    let arr = Array.from(str);
+    let newArr = [];
+    for (let i = 0; i < arr.length; i += 10){
+     newArr.push(arr.slice(i, i + 10));      
+    }
+    let t = newArr.map((item) => {
+      let result = ''
+      for (let i = 0; i < item.length; i++){
+       if (item[i] === '1') {
+         if(item[i + 1] === '1'){
+           result += '-';
+           i+=1;
+         } else result += '.'
+       }
+      }
+      return result
+    })
+    let a = t.map((item) => {
+      if(item === '') {
+        return ' ';
+      } else return MORSE_TABLE[item];
+    })   
+   return a.join('')
+}
 module.exports = {
     decode
 }
